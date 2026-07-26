3e81d424f0a01ad31b7e14459accaa1f9e53ed3b
ibrahimkayum18 <ibrahimkayum017@gmail.com>
2026-07-27 04:25:20 +0600

    removed mongoDB second time

--- index.js
+++ index.js
@@ -19,13 +19,16 @@ app.use(
 // ================= EMAIL SETUP =================
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 transporter.verify((error) => {
